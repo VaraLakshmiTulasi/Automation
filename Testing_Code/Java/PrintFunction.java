773eb834196f745fb1eb04c03dbf142b89ff909e
VaraLakshmiTulasi <laxmitulasi95@gmail.com>
2023-01-05 15:15:06 +0530

    basics of java

---
+++ Testing_Code/Java/PrintFunction.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class PrintFunction {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to java");
+	}
+
+}
---
+++ Testing_Code/Java/UsingVariables.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class UsingVariables
+{
+
+	public static void main(String[] args)
+	{
+	 int i=67;   //Declaration  & Initialization
+	 int j;      //Declaration
+	 j=65;        //Initialization
+	 System.out.println(" The sum Of " + i + " and " + j + " is : " +(i+j));
+	 
+
+	}
+
+}
